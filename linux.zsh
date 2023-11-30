81c447b5f8b0f8befce9ad9d33af0242b8b5470e
Lennard Beers <l.beers@outlook.de>
2023-12-01 00:13:21 +0100

    fix: gh

--- linux.zsh
+++ linux.zsh
@@ -1,8 +1,20 @@
 #!/bin/zsh
 
-echo "Installing zsh..."
+HAS_APT=$(which apt)
 
-sudo apt install zsh
+export NONINTERACTIVE=true
+
+if [ "$HAS_APT" ]; then
+    echo "Installing zsh..."
+    sudo apt install zsh
+fi
+
+HAS_ZSH=$(which zsh)
+
+if [ ! "$HAS_ZSH" ]; then
+    echo "Zsh not found. Please install zsh and run this script again."
+    exit 1
+fi
 
 echo "Installing oh-my-posh..."
 
@@ -26,6 +38,19 @@ echo "Installing oh-my-posh theme..."
 mkdir -p ~/.oh-my-posh/themes
 cp ./eule.omp.json ~/.oh-my-posh/themes/eule.omp.json
 
+echo "Installing linuxbrew..."
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo "Installing GitHub CLI..."
+
+brew install gh
+
 echo "Reloading profile..."
 
 source ~/.zshrc
+
+echo "Installing GitHub Copilot CLI..."
+
+gh extension install github/gh-copilot
+
--- linux/.zshrc
+++ linux/.zshrc
@@ -8,12 +8,19 @@ alias h="history"
 
 export PATH="$HOME/.local/bin:$PATH"
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-source <(ng completion script)
+if [ -s "$(which nvm)" ]; then
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+fi
 
-eval "$(github-copilot-cli alias -- "$0")"
+if [ -s "$(which ng)" ]; then
+    # load ng
+    source <(ng completion script)
+fi
+
+alias '?'='gh copilot explain'
+alias '??'='gh copilot suggest -t shell'
 
 export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
 export DISPLAY=:0;
